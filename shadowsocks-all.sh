7f026d0ed3066770fc92e165bd7d9618e66e5ca7
Zhexiao Lin <72129097+zhexiaolin@users.noreply.github.com>
2025-12-02 18:41:13 -0800

    Add files via upload

--- shadowsocks-all.sh
+++ shadowsocks-all.sh
@@ -1,13 +1,4 @@
-{\rtf1\ansi\ansicpg936\cocoartf2818
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;}
-{\colortbl;\red255\green255\blue255;\red255\green255\blue255;}
-{\*\expandedcolortbl;;\cssrgb\c100000\c100000\c100000;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
-
-\f0\fs26 \cf2 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 #!/usr/bin/env bash\
+#!/usr/bin/env bash\
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin\
 export PATH\
 #\
